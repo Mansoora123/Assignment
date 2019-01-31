21a1315e9853d9413f9375cba7d5db071b7ce2a9
Mansoora123 <47170346+Mansoora123@users.noreply.github.com>
2019-01-31 13:12:19 +0530

    new project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Please enter two numbers for addition");
+                Myservice.WebService1 obj = new Myservice.WebService1();
+                int n1 = Convert.ToInt16(Console.ReadLine());
+                int n2 = Convert.ToInt16(Console.ReadLine());
+
+                string res = obj.Add(n1, n2);
+                Console.WriteLine(res);
+            }
+            {
+                Console.WriteLine("Please enter two numbers for subtraction");
+                int n1 = Convert.ToInt16(Console.ReadLine());
+                int n2 = Convert.ToInt16(Console.ReadLine());
+                Myservice.WebService1 obj = new Myservice.WebService1();
+                Console.WriteLine(obj.Subtarct(n1, n2));
+            }
+            {
+                Console.WriteLine("Please enter two numbers for multiplication");
+                int n1 = Convert.ToInt16(Console.ReadLine());
+                int n2 = Convert.ToInt16(Console.ReadLine());
+                Myservice.WebService1 obj = new Myservice.WebService1();
+                Console.WriteLine(obj.Multiply(n1, n2));
+
+            }
+            Console.WriteLine("Thanks for trying ,visit again");
+
+            Console.ReadKey();
+            
+     
+
+        }
+    }
+}
---
+++ WebApplication1/WebService1.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+     
+        [WebMethod]
+        public string Add(int n1, int n2)
+        {
+            int res = n1 + n2;
+            return "Sum=" + res.ToString();
+        }
+        [WebMethod]
+        public string Subtarct(int n1, int n2)
+        {
+            int res= n1 - n2;
+            return "The result is"+res.ToString();
+        }
+        [WebMethod]
+        public string Multiply(int n1, int n2)
+        {
+            int res = n1 * n2;
+            return "Product=" + res.ToString();
+        }
+    }
+}
